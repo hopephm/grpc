7ad6b2cc5bb503b4baa8117a9b41915a93c956bf
hopephm <hopephm@naver.com>
2025-01-04 21:12:57 +0900

    feat: initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    val kotlinVersion = "2.1.0"
+    val springBootVersion = "3.4.1"
+
+    id("org.springframework.boot") version springBootVersion
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    if (project.childProjects.isNotEmpty()) {
+        return@subprojects
+    }
+
+    group = "kr.hope.grpc"
+    version = "0.0.1-SNAPSHOT"
+
+    apply {
+        plugin("kotlin")
+        plugin("kotlin-kapt")
+        plugin("kotlin-spring")
+        plugin("kotlin-jpa")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    dependencies {
+        implementation(kotlin("reflect"))
+        implementation("org.springframework.boot:spring-boot-starter")
+
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "grpc"
+
+include(
+    "ui:grpc",
+    "protocol",
+    "domain",
+)
+
+makeProjectDir(rootProject, "subprojects")
+
+fun makeProjectDir(project: ProjectDescriptor, group: String) {
+    project.children.forEach {
+        println("$group -> ${it.name}")
+
+        it.projectDir = file("$group/${it.name}")
+        makeProjectDir(it, "$group/${it.name}")
+    }
+}
